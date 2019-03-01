c0a535f6e9f7d8be4bbcb8bd89233dbd396a3fd9
Andrew B <brei0017@gmail.com>
2019-03-01 12:01:07 -0800

    Configure Json Formatting

--- GigHub/GigHub/App_Start/WebApiConfig.cs
+++ GigHub/GigHub/App_Start/WebApiConfig.cs
@@ -1,14 +1,21 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
 
 namespace GigHub
 {
     public static class WebApiConfig
     {
+
         public static void Register(HttpConfiguration config)
         {
+            var settings = GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
